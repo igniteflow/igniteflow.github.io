542152375bb8f58a18d327a4f223256cb86625a2
Phil Tysoe <phil@potatolondon.com>
2019-04-15 09:33:04 +0100

    Import allthethings (typography themes)
    Comment out the rest once one has been selected as THE CHOSEN ONE

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,29 @@
 import Typography from 'typography'
-import fairyGatesTheme from 'typography-theme-fairy-gates'
 
-const typography = new Typography(fairyGatesTheme)
+import altonTheme from 'typography-theme-alton'
+import deYoungTheme from 'typography-theme-de-young'
+import doelgerTheme from 'typography-theme-doelger'
+import elkGlenTheme from 'typography-theme-elk-glen'
+import funstonTheme from 'typography-theme-funston'
+import grandViewTheme from 'typography-theme-grand-view'
+import irvingTheme from 'typography-theme-irving'
+import judahTheme from 'typography-theme-judah'
+import lawtonTheme from 'typography-theme-lawton'
+import lincolnTheme from 'typography-theme-lincoln'  //
+import kirkhamTheme from 'typography-theme-kirkham'
+import moragaTheme from 'typography-theme-moraga'
+import noriegaTheme from 'typography-theme-noriega'
+import oceanBeachTheme from 'typography-theme-ocean-beach'  //
+import parnassusTheme from 'typography-theme-parnassus'
+import stAnnesTheme from 'typography-theme-st-annes'  // needs bigger font
+import stowLakeTheme from 'typography-theme-stow-lake'
+
+import sutroTheme from 'typography-theme-sutro'
+import sternGroveTheme from 'typography-theme-stern-grove'
+import twinPeaksTheme from 'typography-theme-twin-peaks'
+import githubTheme from 'typography-theme-github'  //
+
+const typography = new Typography(lincolnTheme)
 
 export default typography
 export const rhythm = typography.rhythm
