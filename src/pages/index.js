b7b2f3fba3e5cd3b215955eb90bbc79967b12d90
Phil Tysoe <phil@potatolondon.com>
2019-04-15 09:32:18 +0100

    Hide the blog count
    As there's only one blog at the moment, let's skip the list page and
    just display it directly without excerpt

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default ({ data }) => {
   return (
     <Layout>
       <div>
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {/* <h4>{data.allMarkdownRemark.totalCount} Posts</h4> */}
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
             <h3>
